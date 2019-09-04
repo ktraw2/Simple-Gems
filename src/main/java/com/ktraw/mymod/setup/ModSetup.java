af2a56bdc23ac77adb1b80ff1640e17cc3582df9
Kevin Traw <ktraw2@gmail.com>
2019-09-03 21:32:52 -0700

    Add more blockstates for Emerald Lamp and added recipe for Potion of Luck

--- src/main/java/com/ktraw/mymod/setup/ModSetup.java
+++ src/main/java/com/ktraw/mymod/setup/ModSetup.java
@@ -3,10 +3,15 @@ package com.ktraw.mymod.setup;
 import com.ktraw.mymod.blocks.ModBlocks;
 import com.ktraw.mymod.functions.GeneratorCopyNbt;
 import com.ktraw.mymod.functions.GeneratorLore;
+import com.ktraw.mymod.items.ModItems;
 import com.ktraw.mymod.world.OreGeneration;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.*;
 import net.minecraft.world.storage.loot.functions.LootFunctionManager;
+
+import java.lang.reflect.Method;
 
 public final class ModSetup {
     private static ModSetup setup = new ModSetup();
@@ -33,5 +38,14 @@ public final class ModSetup {
         OreGeneration.setupOreGeneration();
         LootFunctionManager.registerFunction(new GeneratorLore.Serializer());
         LootFunctionManager.registerFunction(new GeneratorCopyNbt.Serializer());
+
+        try {
+            Method addMix = PotionBrewing.class.getDeclaredMethod("addMix", Potion.class, Item.class, Potion.class);
+            addMix.setAccessible(true);
+            addMix.invoke(null, Potions.AWKWARD, ModItems.RUBY, Potions.LUCK);
+        }
+        catch (Exception e) {
+            System.err.println("Error adding custom potions.");
+        }
     }
 }
